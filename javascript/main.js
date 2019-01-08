52b2f906541a0da6e3b206adeb5af90fa8b55877
Scott Allan <sallan306@gmail.com>
2019-01-08 03:49:09 -0600

    got a bunch done, need to finish my portfolio links

--- javascript/main.js
+++ javascript/main.js
@@ -41,6 +41,66 @@ $('.img-parallax').each(function(){
 var f = document.getElementById('contentWindow')
 
 $(document).ready(function(){
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("portBtn1");
+var btn2 = document.getElementById("portBtn2");
+var btn3 = document.getElementById("portBtn3");
+var btn4 = document.getElementById("portBtn4");
+var btn5 = document.getElementById("portBtn5");
+var btn6 = document.getElementById("portBtn6");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn1.onclick = () => {
+    $(".modal-content").html("<img class='modal-image' src='images/1-Sunny.png' alt='Sunny Weekend'/>")
+    $(".modal-content").append("<p>Hard time deciding where to travel? Let us pick for you! All you need is a free weekend and a sense of adventure.</p><input type='button' class='modalButtonSunny' value='Sunny Weekend' />")
+    $(".modalButtonSunny").on("click",  () => window.location.href="https://sallan306.github.io/SunnyWeekend/")
+    modal.style.display = "block";
+    }
+btn2.onclick = () => {
+    $(".modal-content").html("<img class='modal-image' src='images/2-Brew.png' alt='Brewstash'/>")
+    $(".modal-content").append("<p>The Brewstash App was designed to improve the user experience of drinking at the many bars in Austin</p><input type='button' class='modalButtonBrew' value='Brewstash' />")
+    $(".modalButtonBrew").on("click",  () => window.location.href="https://brewstash.herokuapp.com/")
+    modal.style.display = "block";
+    }
+btn3.onclick = () => {
+    $(".modal-content").html("<img class='modal-image' src='images/3-Giraffe.png' alt='Giraffe Trivia'/>")
+    $(".modal-content").append("<p>Having a rough day? Want to learn some amazing Giraffe facts that will knock your orange spots off? Play my game!</p><input type='button' class='modalButtonGiraffe' value='Giraffe Trivia' />")
+    $(".modalButtonGiraffe").on("click",  () => window.location.href="https://sallan306.github.io/TriviaGame/")
+    modal.style.display = "block";
+    }
+btn4.onclick = () => {
+    $(".modal-content").html("<img class='modal-image' src='images/3-Giraffe.png' alt='Giraffe Trivia'/>")
+    $(".modal-content").append("<p>Having a rough day? Want to learn some amazing Giraffe facts that will knock your orange spots off? Play my game!</p><input type='button' class='modalButtonGiraffe' value='Giraffe Trivia' />")
+    $(".modalButtonGiraffe").on("click",  () => window.location.href="https://sallan306.github.io/TriviaGame/")
+    modal.style.display = "block";
+    }
+btn5.onclick = () => {
+    $(".modal-content").html("<img class='modal-image' src='images/3-Giraffe.png' alt='Giraffe Trivia'/>")
+    $(".modal-content").append("<p>Having a rough day? Want to learn some amazing Giraffe facts that will knock your orange spots off? Play my game!</p><input type='button' class='modalButtonGiraffe' value='Giraffe Trivia' />")
+    $(".modalButtonGiraffe").on("click",  () => window.location.href="https://sallan306.github.io/TriviaGame/")
+    modal.style.display = "block";
+    }
+btn6.onclick = () => {
+    $(".modal-content").html("<img class='modal-image' src='images/3-Giraffe.png' alt='Giraffe Trivia'/>")
+    $(".modal-content").append("<p>Having a rough day? Want to learn some amazing Giraffe facts that will knock your orange spots off? Play my game!</p><input type='button' class='modalButtonGiraffe' value='Giraffe Trivia' />")
+    $(".modalButtonGiraffe").on("click",  () => window.location.href="https://sallan306.github.io/TriviaGame/")
+    modal.style.display = "block";
+    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => modal.style.display = "none";
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
     $("#aboutMeButton").on("click", function() {
 
         if ($(".aboutMe").data("visible") === false) {
