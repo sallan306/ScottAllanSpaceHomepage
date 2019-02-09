5e73c3cb8561afeccd48960dc2e7815cc7eadadf
Scott Allan <sallan306@gmail.com>
2019-02-08 21:41:41 -0600

    removed url modification

--- javascript/main.js
+++ javascript/main.js
@@ -8,9 +8,7 @@ const   modal = document.getElementById('myModal'),
         width1000 = window.matchMedia("(max-width: 1000px)");
 
 var currentWindow = "none"
-history.pushState({
-    id: 'homepage'
-}, 'Home | My App', 'http://my-app-url.com/?p=homepage');   
+
 function changeContentWindowWidth() {
     if (width450.matches) { 
         console.log("under 450px")
